cdbb0813e3777523253d99362d5edf9c5b43bac3
Josh Burrill <joshua.burrill.1@gmail.com>
2020-08-05 22:42:30 +0100

    Added tests for sign-up/in/out for auth routes

--- auth/src/routes/__test__/sign-in.test.ts
+++ auth/src/routes/__test__/sign-in.test.ts
@@ -1,1 +1,64 @@
-it("Checks for 400 for invalid email and password", () => {});
+import request from "supertest";
+
+import { User } from "../../models/user-model";
+import { app } from "../../app";
+
+// Helper sign-up function
+const signUpHelper = async () => {
+  await request(app).post("/api/v1/sign-up").send({
+    signUpEmail: "test_1jhkfhkdfj@test.com",
+    sendFromEmail: null,
+    password: "password",
+    username: "username",
+  });
+};
+
+describe("Tests for the sign-in route", () => {
+  it("Checks for 400 for invalid email", async () => {
+    await signUpHelper();
+
+    await request(app)
+      .post("/api/v1/sign-in")
+      .send({
+        signUpEmail: "test_1jhkfh.com",
+        password: "password",
+      })
+      .expect(400);
+  });
+
+  it("Checks for 400 for invalid password", async () => {
+    await signUpHelper();
+
+    await request(app)
+      .post("/api/v1/sign-in")
+      .send({
+        signUpEmail: "test_1jhkfhkdfj@test.com",
+        password: "pass",
+      })
+      .expect(400);
+  });
+
+  it("Checks for 401 for invalid email and password", async () => {
+    await signUpHelper();
+
+    await request(app)
+      .post("/api/v1/sign-in")
+      .send({
+        signUpEmail: "test_1jhkfhkdfj@test.com",
+        password: "password",
+      })
+      .expect(401);
+  });
+
+  it("Checks for successful sign-in", async () => {
+    await signUpHelper();
+
+    await request(app)
+      .post("/api/v1/sign-in")
+      .send({
+        signUpEmail: "test_1jhkfhkdfj@test.com",
+        password: "password",
+      })
+      .expect(200);
+  });
+});
